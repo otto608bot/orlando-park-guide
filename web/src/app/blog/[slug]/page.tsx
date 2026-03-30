882f92a54c6f229ab8166086883003d877ae5de2
Rufus (PlanYourPark Scout) <rufus@planyourpark.com>
2026-03-29 21:47:15 -0500

    style: Add blog article CSS matching original design

--- web/src/app/blog/[slug]/page.tsx
+++ web/src/app/blog/[slug]/page.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import "@/app/blog.css";
 
 interface Props {
   params: Promise<{ slug: string }>;
