9f9fe0e880ca05b90381e14fedd8d65a27de5d59
Rufus (PlanYourPark Scout) <rufus@planyourpark.com>
2026-03-29 21:54:50 -0500

    Revert "Feature: Next.js + Sanity Frontend Migration (#26)"
    This reverts commit ef91bc67b9faa0639de15c73811cf36951eff75a.

--- studio/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import post from './post'
-import author from './author'
-import category from './category'
-import seoSuite from './objects/seoSuite'
-import affiliateBlock from './objects/affiliateBlock'
-import tearAway from './objects/tearAway'
-import engagement from './objects/engagement'
-
-export const schemaTypes = [post, author, category, seoSuite, affiliateBlock, tearAway, engagement]
--- web/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import { client } from './sanity'
-import { allPostSlugsQuery } from './queries'
-
-export async function getAllPostSlugs(): Promise<string[]> {
-  return client.fetch(allPostSlugsQuery)
-}
